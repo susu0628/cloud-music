2769b6d184a8ec5472810d3b82b4eb982b019f1a
jiangsusu <jiangsusu7586@dingtalk.com>
2021-11-12 16:59:24 +0800

    添加redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Suspense } from "react";
+import routers from "./routes/index";
+import store from "./store";
+import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';
-import routers from "./routes/index";
 import { GlobalStyle } from './style';
 import { IconStyle } from './assets/iconfont/iconfont'
 
 function App() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <HashRouter>
-        <GlobalStyle />
-        <IconStyle />
-        {
-          renderRoutes(routers)
-        }
-      </HashRouter>
-    </Suspense>
+    <Provider store={store}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <HashRouter>
+          <GlobalStyle />
+          <IconStyle />
+          {
+            renderRoutes(routers)
+          }
+        </HashRouter>
+      </Suspense>
+    </Provider>
+    
   );
 }
 
---
+++ src/store/reducer.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux-immutable';
+
+export default combineReducers({
+  // 添加具体功能模块的reducer
+})
